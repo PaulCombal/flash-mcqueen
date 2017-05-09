6e73388efcdd11db9545074464aa0e58af117afd
DMaxence <maxence.duhoux@viacesi.fr>
2017-05-09 16:31:33 +0200

    add detection function

--- Detect/detection.h
+++ Detect/detection.h
@@ -1,7 +1,37 @@
 #include "Arduino.h"
 
-void forward(int distance, int pin){
-	for (int i = 0; i < distance; ++i)
-	{
-		digitalWrite(pin, HIGH);
-	}
+void detectFront(int pin){
+  int sensorValue = analogRead(pin);
+  //plus l'objet est proche, plus la valeur est grande
+  //inversons la valeur afin d'avoir une distance faible si l'objet est proche
+  float distanceFromObject = 10000/(float)sensorValue;
+
+  return distanceFromObject;
+}
+
+void detectLeft(int pin){
+  int sensorValue = analogRead(pin);
+  //plus l'objet est proche, plus la valeur est grande
+  //inversons la valeur afin d'avoir une distance faible si l'objet est proche
+  float distanceFromObject = 10000/(float)sensorValue;
+
+  return distanceFromObject;
+}
+
+void detectRight(int pin){
+  int sensorValue = analogRead(pin);
+  //plus l'objet est proche, plus la valeur est grande
+  //inversons la valeur afin d'avoir une distance faible si l'objet est proche
+  float distanceFromObject = 10000/(float)sensorValue;
+
+  return distanceFromObject;
+}
+
+void detectBack(int pin){
+  int sensorValue = analogRead(pin);
+  //plus l'objet est proche, plus la valeur est grande
+  //inversons la valeur afin d'avoir une distance faible si l'objet est proche
+  float distanceFromObject = 10000/(float)sensorValue;
+
+  return distanceFromObject;
+}
