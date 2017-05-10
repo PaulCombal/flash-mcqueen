3ed1df66ab5eefa9d229b4c25b58e71e44fb8b43
Paul <abonnementspaul@gmail.com>
2017-05-10 14:38:28 +0200

    Precisions ajoutees

--- course-obstacle/course-obstacle.ino
+++ course-obstacle/course-obstacle.ino
@@ -31,7 +31,19 @@ void setup() {
 void loop() {
   accelgyro.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);  // read measurements from device
 
-
+  // display tab-separated accel/gyro x/y/z values
+  Serial.print("a/g:\t");
+  Serial.print(ax); 
+  Serial.print("\t");
+  Serial.print(ay); 
+  Serial.print("\t");
+  Serial.print(az); 
+  Serial.print("\t");
+  Serial.print(gx); 
+  Serial.print("\t");
+  Serial.print(gy); 
+  Serial.print("\t");
+  Serial.println(gz);
  
   // blink LED to indicate activity
   blinkState = !blinkState;
--- course/course.ino
+++ course/course.ino
@@ -13,6 +13,14 @@ const int defaultSpeed = 255;
 int periode=20000;// période entre chaque début d'impulsion en microsecondes
 const int pinServo=8; // variable pour le pin connecté à la commande du servo
 
+// Detecteurs
+
+// définition des broches utilisées 
+int trig = 8; 
+int echo = 6; 
+long lecture_echo; 
+long cm;
+
 // SETUP =================================
 
 void setup() {
@@ -22,8 +30,11 @@ void setup() {
   pinMode(in2Pin, OUTPUT);
   pinMode(enablePin, OUTPUT);
   pinMode(pinServo, OUTPUT);
+  pinMode(trig, OUTPUT); 
+  pinMode(echo, INPUT); 
 
   digitalWrite(pinServo,LOW);
+  digitalWrite(trig, LOW); 
 
   forward(defaultSpeed);
   
@@ -33,6 +44,20 @@ void setup() {
 // LOOP ==================================
 
 void loop() {
+  forward(defaultSpeed);
+  delay(1000);
+  forward(0);
+
+  digitalWrite(trig, HIGH); 
+  delayMicroseconds(10); 
+  digitalWrite(trig, LOW); 
+  lecture_echo = pulseIn(echo, HIGH); 
+  cm = lecture_echo / 58; 
+  Serial.print("Distancem : "); 
+  Serial.println(cm); 
+  delay(1000); 
+
+  
   if(false) //On détecte un obstacle vers la droite
   {
     setAngle(0);//vers la gauche
